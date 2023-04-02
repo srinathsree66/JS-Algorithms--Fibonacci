468a91fb1630330dca1d79682d8337da10e7756e
sree66 <>
2023-04-02 16:35:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function fibonacci(n){
+  const fib=[0,1];
+  for(let i=2; i<n;i++){
+    fib[i]= fib[i-1] + fib[i-2];
+  }
+  return fib;
+}
+// // Big-O notation is :- O(n)-> Linear
+console.log(fibonacci(2))
+console.log(fibonacci(3))
+console.log(fibonacci(7))
